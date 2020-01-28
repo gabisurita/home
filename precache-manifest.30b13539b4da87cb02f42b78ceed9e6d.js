b9ede43450b69c9cfb1a16064fa2b6f9b4d0685c
Gabriela Surita <gabsurita@gmail.com>
2020-01-27 23:26:31 -0300

    Updates

---
+++ precache-manifest.30b13539b4da87cb02f42b78ceed9e6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f314be864acc412243a854935b166ec9",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "4c589cefa92e3912a163",
+    "url": "/home/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a43d9dbd9b57ce1b302",
+    "url": "/home/static/js/2.15d41c2d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/home/static/js/2.15d41c2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c589cefa92e3912a163",
+    "url": "/home/static/js/main.9312a597.chunk.js"
+  },
+  {
+    "revision": "e27c0f90f1286344ecc1",
+    "url": "/home/static/js/runtime-main.64cdd66f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
